84ae26f6923a79a90b9ed52716b9734069e35417
DavidAkerlind <david.akerlind@icloud.com>
2025-04-22 11:34:35 +0200

    The normal dev version not hash router

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+// 	plugins: [react()],
+// 	base: '/ticketapp-react-exam/',
+// });
+
 export default defineConfig({
 	plugins: [react()],
-	base: '/ticketapp-react-exam/',
+	base: '/',
 });
