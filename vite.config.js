c036b67507e18bd910c8ac991ecf6b6b28016570
DavidAkerlind <david.akerlind@icloud.com>
2025-04-23 12:29:25 +0200

    fix: update Vite configuration to set correct base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-// 	plugins: [react()],
-// 	base: '/ticketapp-react-exam/',
-// });
-
 export default defineConfig({
 	plugins: [react()],
-	base: '/',
+	base: '/ticketapp-react-exam/',
 });
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	base: '/',
+// });
