fa6fd2775fb6ed155dac6114ee61ff14fc222043
DavidAkerlind <david.akerlind@icloud.com>
2025-04-24 10:24:59 +0200

    feat: add compression plugin to Vite configuration for improved build performance

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
 	base: '/ticketapp-react-exam/',
-	plugins: [react(), visualizer({ open: true })],
+	plugins: [
+		react(),
+		visualizer({ open: true }),
+		compression({ algorithm: 'brotliCompress' }), // eller gzip
+	],
 	build: {
 		rollupOptions: {
 			output: {
 				manualChunks(id) {
 					if (id.includes('node_modules')) {
-						if (id.includes('react-router-dom')) {
+						if (id.includes('react-router-dom'))
 							return 'vendor_router';
-						}
-						if (id.includes('react')) {
-							return 'vendor_react';
-						}
+						if (id.includes('react')) return 'vendor_react';
 						return 'vendor';
 					}
 				},
