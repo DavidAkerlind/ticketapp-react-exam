53af1c7ffcf7db072d2aed72d53c2797ee71829a
DavidAkerlind <david.akerlind@icloud.com>
2025-04-24 09:55:07 +0200

    feat: add seat info styling to TicketCard and update global styles for better layout

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
 	base: '/ticketapp-react-exam/',
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (id.includes('react-router-dom')) {
+							return 'vendor_router';
+						}
+						if (id.includes('react')) {
+							return 'vendor_react';
+						}
+						return 'vendor';
+					}
+				},
+			},
+		},
+	},
 });
 
 // export default defineConfig({
